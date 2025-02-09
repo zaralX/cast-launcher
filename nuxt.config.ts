0887d75fc12aaaa038d9d6e5edf9d9c24d4a26db
zaralX <61970128+zaralX@users.noreply.github.com>
2025-02-10 00:57:55 +0800

    nuxt configs fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  ssr: false,
+
   // Global css
   css: ['~/assets/css/tailwind.css'],
 
   vite: {
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      strictPort: true,
+    },
     plugins: [
       tailwindcss(),
     ],
