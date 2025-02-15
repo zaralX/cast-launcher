518919485961181ba3208d968ae1bc9d7b1cafab
zaralX <61970128+zaralX@users.noreply.github.com>
2025-02-16 01:44:21 +0800

    glow

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,8 @@ const route = useRoute()
           <el-tooltip effect="light" placement="right" content="Главная">
             <NuxtLink to="/">
               <button :class="route.path === '/' ? 'text-blue-400 bg-sky-300/[.1]' : 'text-neutral-200'" class="cursor-pointer w-10 h-10 flex justify-center items-center rounded-lg text-lg hover:shadow-lg hover:bg-neutral-700 transition duration-200">
+                <div :class="route.path === '/' ? 'h-6' : 'h-0'" class="w-1 bg-blue-400 absolute left-0 rounded-r-md transition-all duration-200 -translate-x-0.5"></div>
+                <div :class="route.path === '/' ? 'h-8' : 'h-0'" class="w-4 bg-blue-400 absolute left-0 rounded-r-md transition-all duration-200 -translate-x-2 blur-md opacity-50"></div>
                 <i class="pi pi-home"></i>
               </button>
             </NuxtLink>
@@ -18,6 +20,8 @@ const route = useRoute()
           <el-tooltip effect="light" placement="right" content="Тест">
             <NuxtLink to="/test">
               <button :class="route.path === '/test' ? 'text-blue-400 bg-sky-300/[.1]' : 'text-neutral-200'" class="cursor-pointer w-10 h-10 flex justify-center items-center rounded-lg text-lg hover:shadow-lg hover:bg-neutral-700 transition duration-200">
+                <div :class="route.path === '/test' ? 'h-6' : 'h-0'" class="w-1 bg-blue-400 absolute left-0 rounded-r-md transition-all duration-200 -translate-x-0.5"></div>
+                <div :class="route.path === '/test' ? 'h-8' : 'h-0'" class="w-4 bg-blue-400 absolute left-0 rounded-r-md transition-all duration-200 -translate-x-2 blur-md opacity-50"></div>
                 <i class="pi pi-clipboard"></i>
               </button>
             </NuxtLink>
@@ -27,6 +31,8 @@ const route = useRoute()
           <el-tooltip effect="light" placement="right" content="Главная">
             <NuxtLink to="/settings">
               <button :class="route.path === '/settings' ? 'text-blue-400 bg-sky-300/[.1]' : 'text-neutral-200'" class="cursor-pointer w-10 h-10 flex justify-center items-center rounded-lg text-lg hover:shadow-lg hover:bg-neutral-700 transition duration-200">
+                <div :class="route.path === '/settings' ? 'h-6' : 'h-0'" class="w-1 bg-blue-400 absolute left-0 rounded-r-md transition-all duration-200 -translate-x-0.5"></div>
+                <div :class="route.path === '/' ? 'h-8' : 'h-0'" class="w-4 bg-blue-400 absolute left-0 rounded-r-md transition-all duration-200 -translate-x-2 blur-md opacity-50"></div>
                 <i class="pi pi-cog"></i>
               </button>
             </NuxtLink>
