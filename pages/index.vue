c8edcde916bd9a6d06587b986ad699a3e1af8636
zaralX <61970128+zaralX@users.noreply.github.com>
2025-03-25 00:04:45 +0800

    search updated

--- pages/index.vue
+++ pages/index.vue
@@ -29,15 +29,15 @@ const install = async (pack_id) => {
 <!--      </div>-->
 <!--    </div>-->
     <el-scrollbar height="100%">
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div v-for="pack in packs" class="w-full gap-4 border rounded-lg border-neutral-700 shadow-lg bg-neutral-800">
-          <div class="flex gap-4 p-2">
-            <img src="/vite.svg" class="w-16 h-16" alt="">
-            <div>
-              <p class="text-lg font-medium">{{ pack?.cast_pack?.name }}</p>
-              <p class="text-xs">{{ pack }}</p>
+      <div class="grid grid-cols-[repeat(auto-fill,_minmax(10rem,_1fr))] gap-4">
+        <div v-for="pack in packs" class="w-full border rounded-lg border-neutral-700 shadow-lg bg-neutral-800">
+          <div class="p-2">
+            <img src="/vite.svg" class="w-full aspect-square" alt="">
+            <div class="mt-4 mb-2">
+              <p class="text-sm font-medium text-nowrap">{{ pack['cast-pack']?.name }}</p>
+              <p class="text-xs opacity-75">{{ pack['cast-pack']['type'] }} {{ pack['cast-pack']?.version }}</p>
             </div>
-            <div class="flex justify-end items-end">
+            <div class="flex justify-center">
               <el-button v-if="pack['cast-pack']?.installed" type="primary" plain size="small" @click="start(pack['cast-pack']?.id)"><i
                   class="pi pi-play text-xs"></i>  Играть
               </el-button>
--- pages/search.vue
+++ pages/search.vue
@@ -1,35 +1,84 @@
 <script setup>
+import axios from "axios";
+import {invoke} from "@tauri-apps/api/core";
 
-import ModrinthSearch from "~/components/search/ModrinthSearch.vue";
+const versions = ref([])
 
-const selectedModPlace = ref("modrinth")
+const filterSources = ref([])
+const filterVersions = ref([])
 
-const modPlaces = [
-  {
-    label: "Modrinth",
-    value: "modrinth",
-    icon: "/modrinth.svg",
-  },
-  {
-    label: "Curseforge",
-    value: "curseforge",
-    icon: "/curseforge.svg",
-    disabled: true,
+const modrinthSearch = ref({})
+
+onMounted(async () => {
+  const response = await axios.get("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json")
+  versions.value = []
+  for (const version of response.data.versions) {
+    if (version.type === "snapshot") continue;
+    if (["old_beta", "old_alpha"].includes(version.type)) continue;
+    versions.value.push(version.id)
   }
-]
+  await getPacks();
+})
+
+const getPacks = async () => {
+  const versionsString = ",[" + filterVersions.value.map(v => "\"versions:" + v + "\"").join(',') + "]"
+  const url = "https://api.modrinth.com/v2/search?facets=[[\"project_type:modpack\"]"+(versionsString === ',[]' ? '' : versionsString)+"]";
+  console.log(url);
+  modrinthSearch.value = (await axios.get(url)).data
+}
+
+const createAndInstall = async (project) => {
+  await invoke("create_pack", {data: {
+      id: project.slug,
+      name: project.title,
+      type: "modrinth",
+      "modrinth-project-id": project.project_id,
+      "modrinth-project-version": project.latest_version,
+    }});
+}
 </script>
 
 <template>
   <div class="h-full flex flex-col">
-    <el-segmented v-model="selectedModPlace" :options="modPlaces" block>
-      <template #default="scope">
-        <div class="flex items-center justify-center gap-2 p-2">
-          <img :src="scope.item.icon" alt="icon" class="w-4 h-4" />
-          <div class="">{{ scope.item.label }}</div>
+    <div class="flex mt-2 h-full">
+      <div class="w-64 h-full p-2 bg-neutral-800 rounded-xl shadow-md">
+        <div>
+          <p class="font-medium text-lg">Источники</p>
+          <el-checkbox-group :min="1" v-model="filterSources">
+            <el-checkbox checked label="Modrinth" value="modrinth" />
+<!--            need add forge support and make it TODO-->
+            <el-checkbox disabled label="Nazzy projects" value="nazzy" />
+          </el-checkbox-group>
         </div>
-      </template>
-    </el-segmented>
-    <ModrinthSearch />
+        <div>
+          <p class="font-medium text-lg">Версии</p>
+          <el-select
+              v-model="filterVersions"
+              multiple
+              collapse-tags
+              collapse-tags-tooltip
+              placeholder="Select"
+              style="width: 240px"
+              @change="getPacks"
+          >
+            <el-option
+                v-for="item in versions"
+                :key="item"
+                :label="item"
+                :value="item"
+            />
+          </el-select>
+        </div>
+      </div>
+      <el-scrollbar height="100%" class="w-full">
+        <div v-for="hit in modrinthSearch?.hits">
+          <p>hit.title {{hit.title}}</p>
+          <p>hit.description {{hit.title}}</p>
+          <p>hit.project_id {{hit.project_id}}</p>
+          <el-button @click="createAndInstall(hit)">Install</el-button>
+        </div>
+      </el-scrollbar>
+    </div>
   </div>
 </template>
 
