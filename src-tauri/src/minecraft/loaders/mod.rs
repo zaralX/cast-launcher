86353ef1f8677a406bf091a070aeb52c3b4747fb
zaralX <looppersmail@gmail.com>
2026-01-18 11:24:36 +0800

    project refactoring

--- src-tauri/src/minecraft/loaders/mod.rs
+++
@@ -1,8 +0,0 @@
-use crate::utils::http_get_json;
-use crate::{VERSION_MANIFEST_LINK};
-
-pub(crate) mod vanilla;
-pub(crate) mod fabric;
-pub(crate) mod modrinth;
-pub(crate) mod forge;
-pub(crate) mod zapi;
