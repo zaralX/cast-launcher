51b0d54e06ecebf7eedcb2a53c7861a4d56fcd6d
zaralX <looppersmail@gmail.com>
2026-01-22 22:18:42 +0800

    /public in production & logo in toolbar

--- app/layouts/main.vue
+++ app/layouts/main.vue
@@ -36,7 +36,8 @@ const appWindow = getCurrentWindow();
 <template>
 <div class="h-screen w-full">
   <div class="bg-zinc-950/50 h-12 flex">
-    <div>
+    <div class="flex">
+      <NuxtImg src="/logo.png" class="h-12 w-12" />
       <ActiveDownloadingModal />
     </div>
     <div data-tauri-drag-region class="flex-1"></div>
@@ -47,10 +48,10 @@ const appWindow = getCurrentWindow();
     </div>
   </div>
   <div class="flex w-full">
-    <div class="bg-zinc-800/50 w-24">
-      <NuxtLink v-for="link in links" :to="link.to" :class="{'text-sky-500': link.to == route.path}" class="flex flex-col w-24 h-16 items-center justify-center gap-1">
+    <div class="bg-zinc-800/50 w-12">
+      <NuxtLink v-for="link in links" :to="link.to" :class="{'text-sky-500 bg-blue-500/25': link.to == route.path}" class="flex flex-col w-12 h-12 items-center justify-center gap-1">
         <Icon :name="link.icon" class="text-lg" />
-        <p class="text-sm">{{ link.name }}</p>
+<!--        <p class="text-sm">{{ link.name }}</p>-->
       </NuxtLink>
     </div>
     <UScrollArea class="h-[calc(100vh-3rem)] w-full">
